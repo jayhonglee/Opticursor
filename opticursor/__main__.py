6be4449e90a7b7dce2128b055b3b22e39537c65e
Jaehong Lee <ja34luv@gmail.com>
2024-11-11 20:10:25 -0800

    add data

--- opticursor/__main__.py
+++ opticursor/__main__.py
@@ -4,10 +4,10 @@ class Main:
     def __init__(self):
         self.face_mesh_app = FaceMeshApp()
 
-    def start(self):
+    def start(self, withDrawing = True):
         print("Starting Face Mesh Application...")
-        self.face_mesh_app.run()
+        self.face_mesh_app.run(withDrawing)
 
 if __name__ == "__main__":
     main_app = Main()
-    main_app.start()
+    main_app.start(False)
